f571d162ad83642e4062259f2f1dbf31a5774ebf
afzw <fanzhiwencc@gmail.com>
2024-03-17 23:25:34 +0800

    feat: 补充swagger注释内容

--- README.md
+++ README.md
@@ -7,6 +7,8 @@
 ## 参考
 https://github.com/marmotedu/iam/tree/v1.0.0
 
+- [go swagg api文档](https://github.com/swaggo/swag/blob/master/README_zh-CN.md#%E5%A3%B0%E6%98%8E%E5%BC%8F%E6%B3%A8%E9%87%8A%E6%A0%BC%E5%BC%8F)
+
 ## 快速开始
 
 - 启动项目-开发（含热加载）
@@ -14,3 +16,5 @@ https://github.com/marmotedu/iam/tree/v1.0.0
   ```sh
   air
   ```
+
+
--- api/user.go
+++ api/user.go
@@ -14,10 +14,14 @@ type GetUserParam struct {
 
 type UserApi struct{}
 
-// @title 获取用户
+// @title 查询用户
+// @router /users/:id [get]
 // @version 1.0
+// @description 依据用户的id，查询用户的信息
+// @summary 查询用户信息
 // @accept json
-// @router /users [get]
+// @param id path string true "用户id"
+// @success 200 {object} model.User
 func (UserApi) GetUser(c *gin.Context) {
 	params, _, _ := middleware.Validate[GetUserParam, any, any](c)
 
