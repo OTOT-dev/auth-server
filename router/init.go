8c09faed51091a1ab0accb09116823d637b60a53
afzw <fanzhiwencc@gmail.com>
2024-03-24 23:35:20 +0800

    perf: 使用变量代替字符串字面量

--- router/init.go
+++ router/init.go
@@ -17,9 +17,16 @@ import (
 )
 
 var (
-	apiUser api.UserApi
 	apiAuth api.AuthApi
+	apiUser api.UserApi
 )
+
+var (
+	authRoutesPrefix    = "/auth"
+	serviceRoutesPrefix = "/api/v1"
+)
+
+var sessionName = "sid"
 
 func InitRouter() {
 	//是否开启debug模式
@@ -32,9 +39,8 @@ func InitRouter() {
 
 	if config.DebugMode {
 		engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+		docs.SwaggerInfo.BasePath = authRoutesPrefix
 	}
-
-	docs.SwaggerInfo.BasePath = "/api/v1"
 
 	// session 设置
 	store := sessions.NewCookieStore([]byte(config.SessionSecret))
@@ -42,16 +48,16 @@ func InitRouter() {
 		Path:   "/",
 		MaxAge: config.SessionExpire, // 设置超时时间为一个小时
 	})
-	engine.Use(sessions.Sessions("sid", store))
+	engine.Use(sessions.Sessions(sessionName, store))
 
 	// 日志设置
 	engine.Use(middleware.LogMiddleware())
 
 	// 登陆认证相关路由
-	authRouterGroup := engine.Group("/auth")
+	authRouterGroup := engine.Group(authRoutesPrefix)
 	authRouter(authRouterGroup)
 	// 业务路由
-	serviceRouterGroup := engine.Group("/api/v1")
+	serviceRouterGroup := engine.Group(serviceRoutesPrefix)
 	serviceRouterGroup.Use(middleware.SessionAuth())
 	userRouter(serviceRouterGroup)
 
